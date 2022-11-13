2bda92b3486a9569b5c34f1c0581ff019ee2b949
zahid1905 <superpichi.chido@gmail.com>
2022-11-12 21:10:47 -0600

    new file:   simple_device.py 	modified:   states.py 	.gitignore

--- states.py
+++ states.py
@@ -4,7 +4,7 @@ from time import sleep
 
 class Iniciar(State):
     """
-    Estado donde inicializamos todos los servicios.
+    Inicializar todos los servicios.
     """
     def on_event(self, event):
         # Iniciar I2C
@@ -15,22 +15,50 @@ class Iniciar(State):
         print("iniciando RFID")
         print("Iniciando Display")
 
-        return EsperandoPago()
+        return EsperandoPago.on_event(self, 'inicio_correcto')
 
 class EsperandoPago(State):
     """
-    Estado donde inicializamos todos los servicios.
+    Esperar por un "pago" en el modulo RFID.
     """
     def on_event(self, event):
+        nfc = ''
         if event == 'inicio_correcto' or event == 'compra_exitosa':
+            
             while nfc != '1':
-                # Placeholder para la comunicacion con el NFC
+                # Placeholder para la comunicacion con el RFID
                 nfc = input("Lectura nfc:")
                 sleep(2)
-            return LecturaCamara()
+
+            return LecturaCamara().on_event(self, 'pago_recibido')
+        
         return self
 
 class LecturaCamara(State):
+    """
+    Obtener el producto con la RPi/Camara.
+    """
     def on_event(self, event):
+        if event == 'pago_recibido':
+            # Placeholder para la comunicacion con la RPi/Camara
+            producto = input("Lectura camara:") # p_[1,2,3,4]
+            sleep(2)
+            return
 
+        return self
+
+class ConfirmarCamara(State):
+    def on_event(self, event):
+        return self
+
+class ActivarMotor(State):
+    def on_event(self, event):
+        return self
+
+class LecturaSensor(State):
+    def on_event(self, event):
+        return self
+
+class FinalizarError(State):
+    def on_event(self, event):
         return self
